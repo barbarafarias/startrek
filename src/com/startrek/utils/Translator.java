91c79631496d52ec190751e40de65f0af93ef1c2
barbarafarias <barbara.fs@gmail.com>
2017-11-10 14:50:25 -0200

    adding maven build and documentation

---
+++ src/com/startrek/utils/Translator.java
@@ -0,0 +1,10 @@
+package com.startrek.utils;
+
+public class Translator {
+
+	public static String translateEnglishName(String englishName) {
+		
+		return "0xF8E5 0xF8D6 0xF8E5 0xF8E1 0xF8D0";
+		
+	}
+}
